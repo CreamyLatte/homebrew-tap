f21b7fbc7b19cb79bc95e1efcd70739f92d4082f
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-11-02 17:35:42 +0900

    Update wateco.rb
    wateco version update.

--- Formula/wateco.rb
+++ Formula/wateco.rb
@@ -1,7 +1,7 @@
 class Wateco < Formula
   desc "This application can convert from audio file to text format or from text file to audio format."
   homepage "https://github.com/CreamyLatte/wateco"
-  url "https://github.com/CreamyLatte/wateco/releases/download/0.1.0/wateco"
+  url "https://github.com/CreamyLatte/wateco/releases/download/0.1.1/wateco"
   sha256 "de4da8960b38367daed4734650d4372655da46b672ceb5abd909a36c6e01bcec"
   license "MIT license"
 
